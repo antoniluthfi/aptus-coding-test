1541c5047a1f247a8fbedb7458a6ab8f3b1ad693
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-10-28 00:26:20 +0800

    feat: setup font family

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,3 +4,24 @@ export const colors = {
     '900': "#3D3D3D"
   }
 }
+
+export const fonts = {
+  'DMSans-Black': 'DMSans-Black',
+  'DMSans-BlackItalic': 'DMSans-BlackItalic',
+  'DMSans-Bold': 'DMSans-Bold',
+  'DMSans-BoldItalic': 'DMSans-BoldItalic',
+  'DMSans-ExtraBold': 'DMSans-ExtraBold',
+  'DMSans-ExtraBoldItalic': 'DMSans-ExtraBoldItalic',
+  'DMSans-ExtraLight': 'DMSans-ExtraLight',
+  'DMSans-ExtraLightItalic': 'DMSans-ExtraLightItalic',
+  'DMSans-Italic': 'DMSans-Italic',
+  'DMSans-Light': 'DMSans-Light',
+  'DMSans-LightItalic': 'DMSans-LightItalic',
+  'DMSans-Medium': 'DMSans-Medium',
+  'DMSans-MediumItalic': 'DMSans-MediumItalic',
+  'DMSans-Regular': 'DMSans-Regular',
+  'DMSans-SemiBold': 'DMSans-SemiBold',
+  'DMSans-SemiBoldItalic': 'DMSans-SemiBoldItalic',
+  'DMSans-Thin': 'DMSans-Thin',
+  'DMSans-ThinItalic': 'DMSans-ThinItalic'
+}
